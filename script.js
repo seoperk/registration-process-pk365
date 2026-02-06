276f0c9644e2764b35b46b6c47d214ce4daf4755
seoperk <usamali2014@gmail.com>
2026-02-06 13:06:14 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+zdocument.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener("click", (e)=>{
+    const id = a.getAttribute("href");
+    const el = document.querySelector(id);
+    if(!el) return;
+    e.preventDefault();
+    el.scrollIntoView({behavior:"smooth", block:"start"});
+    history.replaceState(null, "", id);
+  });
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
